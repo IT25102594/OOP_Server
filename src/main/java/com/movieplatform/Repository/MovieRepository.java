c63b01b9cdf11e0ba332227b6b869a2ad1d2216a
it25101788 <it25101788@my.sliit.lk>
2026-05-08 09:22:19 +0530

    Initialized the movie repository

--- src/main/java/com/movieplatform/Repository/MovieRepository.java
+++ src/main/java/com/movieplatform/Repository/MovieRepository.java
@@ -1,4 +1,8 @@
 package com.movieplatform.Repository;
 
-public interface MovieRepository  {
+import com.movieplatform.Entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
 }
