d75b9c1b288e50a612e2bf4002379a15490cf9a2
Thuvaraga S.A IT25100046 <it25100046@my.sliit.lk>
2026-05-15 14:13:27 +0530

    rental

--- src/main/java/com/movieplatform/Repository/RentalRepository.java
+++ src/main/java/com/movieplatform/Repository/RentalRepository.java
@@ -1,4 +1,48 @@
 package com.movieplatform.Repository;
 
-public interface RentalRepository {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RentalRepository {
+
+    private final String FILE_PATH = "src/main/resources/rentals.txt";
+//add new rent
+    public void saveRental(String rentalData) {
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(FILE_PATH, true)))) {
+            out.println(rentalData);
+        } catch (IOException e) {
+            System.err.println("error: " + e.getMessage());
+        }
+    }
+
+    // 2. read all rent
+    public List<String> getAllRentals() {
+        List<String> rentals = new ArrayList<>();
+        File file = new File(FILE_PATH);
+
+        if (!file.exists()) return rentals;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                rentals.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading files: " + e.getMessage());
+        }
+        return rentals;
+    }
+
+    // 3. to find rent for specific user id
+    public List<String> findByUserId(String userId) {
+        List<String> userRentals = new ArrayList<>();
+        for (String record : getAllRentals()) {
+            if (record.contains("| " + userId + " |")) {
+                userRentals.add(record);
+            }
+        }
+        return userRentals;
+    }
 }
