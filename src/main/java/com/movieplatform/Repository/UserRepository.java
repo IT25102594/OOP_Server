bf74610441c4c3a8a6752f39169d80d75bc0ec40
IT25102594 <it25102594@my.sliit.lk>
2026-05-17 21:47:33 +0530

    Data type change
    Made admin field data type from Byte to Integer
    Added new method for the Admin component

--- src/main/java/com/movieplatform/Repository/UserRepository.java
+++ src/main/java/com/movieplatform/Repository/UserRepository.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByGmail(@Size(max = 45) @NotNull String gmail);
+    User findByGmail(String gmail);
+    List<User> findByAdmin(Integer admin); // handy for listing by role
 }
