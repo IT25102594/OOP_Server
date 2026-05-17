476f527094249d495aa57f61b3b48b7d6124a5d8
it25101788 <it25101788@my.sliit.lk>
2026-05-17 15:13:36 +0530

    Entity changes
    Did some Data base changes

--- src/main/java/com/movieplatform/Entity/Movie.java
+++ src/main/java/com/movieplatform/Entity/Movie.java
@@ -1,22 +1,18 @@
 package com.movieplatform.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Instant;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "movie")
 public class Movie {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -34,28 +30,27 @@ public class Movie {
     @Column(name = "country", length = 45)
     private String country;
 
-    // Movie duration
-    @Column(name = "hours")
-    private Integer hours;
+    @Column(name = "duration")
+    private String duration;
 
-    @Size(max = 255)
-    @Column(name = "shortdescription", length = 255)
+    @Size(max = 45)
+    @Column(name = "shortdescription", length = 45)
     private String shortdescription;
 
-    @Size(max = 1000)
-    @Column(name = "description", length = 1000)
+    @Size(max = 255)
+    @Column(name = "description")
     private String description;
 
     @Size(max = 255)
-    @Column(name = "image", length = 255)
+    @Column(name = "image")
     private String image;
 
     @Size(max = 255)
-    @Column(name = "link", length = 255)
+    @Column(name = "link")
     private String link;
 
     @Size(max = 255)
-    @Column(name = "trailerlink", length = 255)
+    @Column(name = "trailerlink")
     private String trailerlink;
 
     @Column(name = "imdb")
@@ -66,4 +61,11 @@ public class Movie {
 
     @Column(name = "price")
     private Double price;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+
 }
