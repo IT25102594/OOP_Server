e90f0685797d6a9f0982a9df7c0cfe4516e0e314
it25101788 <it25101788@my.sliit.lk>
2026-05-18 12:26:40 +0530

    Made a new category entity

--- src/main/java/com/movieplatform/Entity/Category.java
+++ src/main/java/com/movieplatform/Entity/Category.java
@@ -8,19 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "category", schema = "oop_db")
+@Table(name = "category")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
     @Size(max = 45)
     @Column(name = "name", length = 45)
     private String name;
-
-    @Lob
-    @Column(name = "img")
-    private String img;
 
 
 }
